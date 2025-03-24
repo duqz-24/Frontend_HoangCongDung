71e40f4cc0af3b56db8cb119b2695da0d989d8d5
duqz-24 <hoangcongdung2006@gmail.com>
2025-03-24 09:17:45 +0700

    Delete Seasion20 21-03-2025 (buổi18) directory

--- Seasion20 21-03-2025 (buổi18)/baitap1/scripts.js
+++
@@ -1,9 +0,0 @@
-let n = parseInt(prompt("Nhập số N:"));
-
-let tong = 0;
-
-for (let i = 1; i <= n; i++) {
-  tong += i;
-}
-
-console.log("Tổng các số từ 1 đến " + n + " là: " + tong);
--- Seasion20 21-03-2025 (buổi18)/baitap2/scripts.js
+++
@@ -1,7 +0,0 @@
-let n = parseInt(prompt("Nhập số N:"));
-
-for (let i = 1; i <= n; i++) {
-  if (i % 5 === 0) {
-    console.log(i);
-  }
-}
